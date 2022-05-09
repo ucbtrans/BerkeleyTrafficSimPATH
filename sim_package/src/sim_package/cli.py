7dfe089b62eeb3413fc0d28e65d066b7aff28422
abhinavDhulipala <abhinav.dhulipala@berkeley.edu>
2022-05-09 12:58:10 -0700

    commandline scripts

---
+++ sim_package/src/sim_package/cli.py
@@ -0,0 +1,13 @@
+from runner import Runner
+import argparse
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='command line tool for running spatial queue model')
+    parser.add_argument('--nodes', required=True, help='path to nodes csv that represents all the intersections of your model')
+    parser.add_argument('--links', required=True, help='path to link csv')
+    parser.add_argument('--ods', required=True, help='path to travel demand csv')
+    args = parser.parse_args()
+
+    runner = Runner(args.links, args.nodes, args.ods)
+    runner.spatial_queue_simulation('')
