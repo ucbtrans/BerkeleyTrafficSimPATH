c884f9a83d25e658fb3a4608d7bebb9c1a13d06e
abhinavDhulipala <abhinav.dhulipala@berkeley.edu>
2022-05-15 20:08:31 -0700

    cli added

--- sim_package/src/sim_package/cli.py
+++ sim_package/src/sim_package/cli.py
@@ -7,7 +7,9 @@ if __name__ == '__main__':
     parser.add_argument('--nodes', required=True, help='path to nodes csv that represents all the intersections of your model')
     parser.add_argument('--links', required=True, help='path to link csv')
     parser.add_argument('--ods', required=True, help='path to travel demand csv')
+    parser.add_argument('--name', default='berkeley-evac', help='path to travel demand csv')
     args = parser.parse_args()
 
     runner = Runner(args.links, args.nodes, args.ods)
-    runner.spatial_queue_simulation('')
+    runner.init_sq_simulation()
+    runner.spatial_queue_simulation(args.name)
